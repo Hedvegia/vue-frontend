5b877ee31c74b427851ba1959e391076151d6abe
Hedi <hedi9507@gmail.com>
2018-10-16 14:15:50 +0200

    hello world

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+Vue.use(VueApollo)
+
+new Vue({
+  el: '#app',
+  apolloProvider,
+  render: h => h(App)
+})
